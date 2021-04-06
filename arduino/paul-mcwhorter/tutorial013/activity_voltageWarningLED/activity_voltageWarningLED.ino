46c4cb7123118a6921c9364d859aba5ff29157a1
Neil John D. Ortega <neiljohn.ortega+git@gmail.com>
2021-04-06 16:22:07 +0900

    Control LED potentiometer (Tutorial #13)

--- arduino/paul-mcwhorter/tutorial013/activity_voltageWarningLED/activity_voltageWarningLED.ino
+++ arduino/paul-mcwhorter/tutorial013/activity_voltageWarningLED/activity_voltageWarningLED.ino
@@ -1,13 +1,16 @@
 int readPin = A0;
 int readVal = 0;
 float readConversion = 5./1023.;
-float V = 0.;
-int delayMS = 1000;
+float V = 0;
+float THRESHOLD_VOLTAGE = 4.;
+int delayMS = 500;
+int warningLED = 9;
 
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(readPin, INPUT);
+  pinMode(warningLED, OUTPUT);
   Serial.begin(9600);
 }
 
@@ -18,5 +21,15 @@ void loop() {
   Serial.print("Voltage = ");
   Serial.print(V);
   Serial.println(" V");
+
+  if (V > THRESHOLD_VOLTAGE) {
+    Serial.print("Voltage above threshold of ");
+    Serial.print(THRESHOLD_VOLTAGE);
+    Serial.println(" V");
+    digitalWrite(warningLED, HIGH);
+  } else {
+    digitalWrite(warningLED, LOW);
+  }
+
   delay(delayMS);
 }
