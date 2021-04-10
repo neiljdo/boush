8a1c573f35abae2c79a059c1aac1cf7a57f27d54
Neil John D. Ortega <neiljohn.ortega+git@gmail.com>
2021-04-10 16:30:58 +0900

    Add code for dimmable LED (Tutorial #14)

---
+++ arduino/paul-mcwhorter/tutorial014/activity_dimmableLED/activity_dimmableLED.ino
@@ -0,0 +1,23 @@
+int readPin = A0;
+int readVal = 0;
+// Analog write is [0, 255]
+// Analog read is [0, 1023]
+float readConversion = 255./1023.;
+float V = 0;
+int greenLED = 9;
+int delayMS = 50;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(readPin, INPUT);
+  pinMode(greenLED, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  readVal = analogRead(readPin);
+  analogWrite(greenLED, readVal * readConversion);
+  delay(delayMS);
+}
