2e0ca1ebb4e1b490b2ca824c4d6b2ee770d73d89
Neil John D. Ortega <neiljohn.ortega+git@gmail.com>
2021-04-10 18:47:56 +0900

    Read from serial monitor (Tutorial #19)

---
+++ arduino/paul-mcwhorter/tutorial019/activity_readStrFromSerialMonitor/activity_readStrFromSerialMonitor.ino
@@ -0,0 +1,62 @@
+String promptColor = String("Enter which color LED to blink\n") \
+                   + String("(red / yellow / green):");
+String promptNum = "Enter the number of times to blink LED:";
+int num;
+String color;
+int targetLED;
+int redLED = 11;
+int yellowLED = 10;
+int greenLED = 9;
+int delayMS = 250;
+
+
+void blinkLED(int pin) {
+  digitalWrite(pin, HIGH);
+  delay(delayMS);
+  digitalWrite(pin, LOW);
+  delay(delayMS);
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(redLED, OUTPUT);
+  pinMode(yellowLED, OUTPUT);
+  pinMode(greenLED, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println(promptColor);
+  // Do nothing while waiting for user input
+  while (Serial.available() == 0);
+  color = Serial.readString();
+  color = color.substring(0, color.length() - 1);
+  
+  Serial.println(color.length());
+  if (color == "red") {
+    targetLED = redLED;
+  } else if (color == "yellow") {
+    targetLED = yellowLED;
+  } else if (color == "green") {
+    targetLED = greenLED;
+  } else {
+    targetLED = NULL;
+  }
+
+  if (targetLED) {
+    Serial.println(promptNum);
+    // Do nothing while waiting for user input
+    while (Serial.available() == 0);
+
+    // Blink LED the specified number of times
+    num = Serial.parseInt();
+    while (num > 0) {
+      blinkLED(targetLED);
+      num--;
+    }
+  } else {
+    Serial.println("Invalid color. Please try again.");
+  }
+
+}
